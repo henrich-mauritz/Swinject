5c941fe31f86e7342115e139438630915f4882b3
Henrich Mauritz <henrich.mauritz@icloud.com>
2023-07-20 10:33:39 +0200

    2.8.9

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Swinject",
-            url: "https://github.com/henrich-mauritz/Swinject/releases/download/2.8.8/Swinject.xcframework.zip",
-            checksum: "640a9d9021e0f8b8bf0b41361608c9d640ae653d4c749245ef0fcd4b056596a7"
+            url: "https://github.com/henrich-mauritz/Swinject/releases/download/2.8.9/Swinject.xcframework.zip",
+            checksum: "82a4390d811c124e8c618d02da014e41fabbc40cccc23c63d176f1f3e7cd69b3"
         )
     ]
 )
